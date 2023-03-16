7454b78e72983fe89c030daf6be9172c8ef5afa2
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-16 13:21:44 -0500

    feat: add the basic intro for the website

---
+++ src/components/Intro.jsx
@@ -0,0 +1,23 @@
+export default function Intro() {
+  return (
+    <>
+      <section
+        id="intro"
+        className="flex items-center justify-center flex-col pb-6"
+      >
+        <div className="text-center pr-4 pl-4 lg:pr-40  lg:pl-40">
+          <h1 className="text-4xl font-bold md:text-6xl dark:text-white mb-8">
+            Vaibhav Venkat
+          </h1>
+          <p className="text-base md:text-xl font-medium mb-4"> Backend, Frontend and Devops Engineer </p>
+          <p className="text-sm mb-4 font-semibold">
+            I'm a fullstack developer with knowledge in web development, backend, devops, and some mobile programming with swift.
+            I learned to code by starting new projects and doing new things.
+            I  am most proficient in Typescript and Go, and I am learning Haskell to learn about functional programming, and Swift for IOS Apps.
+            All my programming projects are on my <a href="https://github.com/vibovenkat123" alt="github link" target="_blank" rel="noreferrer noopener"> Github </a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
