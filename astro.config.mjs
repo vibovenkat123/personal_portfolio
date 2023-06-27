e4da86bb6b34d9bb6e34132ce214a94e5ea56b76
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-06-27 18:18:31 -0500

    feat: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  output: "server",
+  adapter: vercel()
 });
