355bbca2e3a375e28e25c5e6f55ec756abc24364
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-06-27 18:10:36 -0500

    feat: add base

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+import type { Project } from "../content";
+
+// props
+const props = defineProps({
+  projects: {
+    type: Array as PropType<Project[]>,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: true,
+  },
+});
+const index = props.index;
+const isDivisable = props.index % 2 == 0;
+const projects = props.projects as Project[];
+</script>
+
+<template>
+  <div v-if="isDivisable" class="grid_item">
+    <div class="project_item">
+      <a
+        :href="`${projects[index].link}`"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        {{ projects[index].name }}
+      </a>
+      <p>{{ projects[index].description }}</p>
+    </div>
+    <div class="project_item" v-if="index < props.projects.length - 1">
+      <a
+        :href="`${projects[index + 1].link}`"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        {{ projects[index + 1].name }}
+      </a>
+      <p>{{ projects[index + 1].description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .grid_item {
+    gap: 2em;
+    width: 100%;
+    display: flex;
+  }
+  @media (max-width: 612px) {
+    .grid_item {
+      gap: em;
+    }
+  }
+  .project_item {
+    width: 75%;
+  }
+</style>
---
+++ src/content.ts
@@ -0,0 +1,38 @@
+export type Project = {
+    name: string,
+    description: string,
+    link: string,
+}
+
+export const projects = [
+    {
+        name: "Review GPT",
+        description: "An automated code review tool that uses GPT",
+        link: "https://github.com/vibovenkat123/review-gpt"
+    },
+    { 
+        name: "Pennyearned",
+        description: "A simple expense manager app built with Swift and Go",
+        link: "https://github.com/vibovenkat123/pennyearned-mobile"
+    },
+    { 
+        name: "Pioneer",
+        description: "A website designed to share images with other people",
+        link: "https://github.com/vibovenkat123/pioneer"
+    },
+    {
+        name: "vaibhavvenkat.com",
+        description: "My personal website (you are here!)",
+        link: "https://github.com/vibovenkat123/personal_portfolio"
+    },
+    {
+        name: "YADBMT",
+        description: "A postgres database migration tool built with Rust",
+        link: "https://github.com/vibovenkat123/yadbmt"
+    },
+    {
+        name: "Pennyearned server",
+        description: "A server for Pennyearned built with Go",
+        link: "https://github.com/vibovenkat123/pennyearned-server"
+    },
+]
