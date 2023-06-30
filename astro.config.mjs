b4f5997f3edf614b17ec0257ccee71888f496bbe
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-06-30 14:42:33 -0500

    feat: add first blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), mdx()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/components/Code.vue
@@ -0,0 +1,34 @@
+<template>
+  <code
+  >
+    <slot />
+  </code>
+</template>
+
+<style>
+code::before,
+code::after {
+  content: "`"
+}
+
+code {
+  color: #9ca3af;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  border-radius: 0.25rem;
+}
+
+pre {
+  background-color: #232323 !important;
+  overflow-x: auto;
+  padding: 1rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  border-radius: 0.25rem;
+}
+
+pre code::before,
+pre code::after {
+  content: none;
+}
+</style>
